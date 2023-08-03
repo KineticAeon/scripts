4acde6747f1bd1ac96441cfcaa6a8458a7f1623a
Aeon <aeoncaptain@gmail.com>
2023-08-03 16:22:34 -0600

    update minecraft server script

--- startMinecraftServer.sh
+++ startMinecraftServer.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
 # FUCK THIS SCRIPT I'M GONNA GO LEARN PYTHON
+# (after realizing python and rust were both too big for this, she gave up)
 
 # automated minecraft server start script
-# made with love by nia <3
-# https://github.com/KineticAeon/NSAS
+# needs wget and jq
+# made with love by nia <3, 2023
+# https://github.com/KineticAeon/NAS
 
-# set server version here
+# set server version and ram (in gb) here
 VERSION="spigot-1.20.1"
+RAM="1"
 
-mv -t ./otherVersions $(find *.jar | sed 's/[^ ]*spigot-1.20.1[^ ]*//ig') 
+mv -t ./otherVersions $(find *.jar | sed "s/[^ ]*$VERSION[^ ]*//ig") 
 
-if [ -f ./otherVersions/$VERSION.jar ]; then
-    mv ./otherVersions/$VERSION.jar ./$VERSION.jar
+if [ -f ./otherVersions/${VERSION}.jar ]; then
+    mv ./otherVersions/${VERSION}.jar ./${VERSION}.jar
+fi
 
 echo "bruh this script is fucking gay why did you make it."
 echo "because I am gay. duh."
-java -Xms1G -Xmx1G -XX:+UseG1GC -jar $VERSION.jar nogui
+java -Xms1G -Xmx${RAM}G -XX:+UseG1GC -jar ${VERSION}.jar nogui
+
